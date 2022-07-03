1a3b2e32402dd91a83559fb554a4ad6fbc0d88a0
Kevin McAleer <kevinmcaleer@gmail.com>
2022-07-03 14:55:34 +0100

    added burger-mqtt

--- burger-mqtt.py
+++ burger-mqtt.py
@@ -4,10 +4,10 @@
 
 from burgerbot import Burgerbot
 from secret import ssid, password
-from umqttsimply import MQTTClient
+from umqttsimple import MQTTClient
 from machine import Pin
 import network
-from time import sleep
+from time import sleep, ticks_ms
 
 mqtt_broker = "192.168.1.152"
 client_id = "burgerbot"
@@ -19,37 +19,33 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
 
-# MOTOR_1 = Pin(6, Pin.OUT)
-# MOTOR_2 = Pin(26, Pin.OUT)
-# MOTOR_1_SPEED = Pin(7, Pin.OUT)
-# MOTOR_2_SPEED = Pin(27, Pin.OUT)
+led = Pin("LED", Pin.OUT)
+led.on()
 
 bot = Burgerbot()
 
 def sub_cb(topic, msg):
-#     global MOTOR_1, MOTOR_2, MOTOR_1_SPEED, MOTOR_2_SPEED
+    """ Subscribe call back """
     global bot
     
     msg = msg.decode("utf-8")
     topic = topic.decode("utf-8")
-    """ Subscribe call back """
+    
+    
     print(f'topic {topic}, msg {msg}')
+    
     if topic == left_motor_topic:
         print(f"move left {float(msg)}")
-#         MOTOR_1_SPEED = float(msg)
-#         MOTOR_1 = 1
         bot.left_motor(float(msg))
+        
     if topic == right_motor_topic:
         print(f"move right {float(msg)}")
-#         MOTOR_2_SPEED = float(msg)
-#         MOTOR_2 = 1
         bot.right_motor(float(msg))
                        
 
 def connect_and_subscribe():
     """ Connect to the MQTT broker and subscribe to the topic"""
     global client_id, mqtt_broker
-    
     
     print(client_id, mqtt_broker, left_motor_topic, right_motor_topic)
     client = MQTTClient(client_id, mqtt_broker, keepalive=30)
@@ -67,9 +63,13 @@ def restart_reconnect():
     client = connect_and_subscribe()
     
 # Connect to wifi
+timer = ticks_ms()
 print("Connecting to Wifi...")
 while wlan.isconnected() == False:
-    pass
+    current_time = ticks_ms()
+    if current_time >= (timer + 250):
+        timer = ticks_ms()
+        led.toggle()
     
 print("Connected")
 print(wlan.ifconfig())
@@ -80,9 +80,15 @@ except OSError as e:
     client = None
     restart_reconnect()
 
+
 while True:
     try:
         client.check_msg()
+        current_time = ticks_ms()
+        if current_time >= (timer + 1000):
+            timer = ticks_ms()
+            print(timer, current_time)
+            led.toggle()
             
     except OSError as e:
         restart_reconnect()
